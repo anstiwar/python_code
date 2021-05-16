90d0ba45a525b50a639a2baf51ca1630a017937c
anstiwar <anshumantiwari12@gmail.com>
2021-05-16 21:24:01 +0530

    Create queue_ll.py

---
+++ DataStructure/queue_ll.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+        # return len(self.items)==0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+
+if __name__ == "__main__":
+    q = Queue()
+    print(q)
+    print(q.is_empty())
+    q.enqueue(10)
+    q.enqueue(20)
+    q.enqueue(30)
+    q.enqueue(40)
+    print(q)
+    q.dequeue()
+    print(q)

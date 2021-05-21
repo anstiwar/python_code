383531ac16bff881fe76f3fb7a0e36fd661fc056
anstiwar <anshumantiwari12@gmail.com>
2021-05-21 15:34:17 +0530

    Update DFS_2.py
    Improved code

--- DataStructure/DFS_2.py
+++ DataStructure/DFS_2.py
@@ -8,15 +8,27 @@ graph = {
     'F' : []
 }
 
-visited = set() # Set to keep track of visited nodes.
+# graph1 = {
+#     'A' : ['B','S'],
+#     'B' : ['A'],
+#     'C' : ['D','E','F','S'],
+#     'D' : ['C'],
+#     'E' : ['C','H'],
+#     'F' : ['C','G'],
+#     'G' : ['F','S'],
+#     'H' : ['E','G'],
+#     'S' : ['A','C','G']
+# }
 
-def dfs(visited,graph,node):
+
+def dfs(graph,node,visited=[]):
     if node not in visited:
-        print(node)
         visited.add(node)
-        for neighbour in graph[node]:
-            dfs(visited,graph,neighbour)
+        if node in graph:
+            for neighbour in graph[node]:
+                dfs(visited,graph,neighbour)
+            return visited
 
 
 if __name__=="__main__":
-    dfs(visited,graph,"A")
+    dfs(graph,"A")
